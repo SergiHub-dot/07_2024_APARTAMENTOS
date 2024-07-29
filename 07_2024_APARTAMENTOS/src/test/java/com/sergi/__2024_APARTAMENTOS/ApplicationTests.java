604b06ea80593c120a456fb5f0056aab5a6ce116
Sergi <Sergi@DESKTOP-CH482ER>
2024-07-29 16:39:05 +0200

    INICIANDO PROYECTO MAVEN CON SPRING

---
+++ 07_2024_APARTAMENTOS/src/test/java/com/sergi/__2024_APARTAMENTOS/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergi.__2024_APARTAMENTOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
